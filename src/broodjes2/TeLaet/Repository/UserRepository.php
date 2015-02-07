8a8fb7270e2c337743353c16c80dedf554764282
Beerfleet <devel.janvb@gmail.com>
2015-02-07 13:45:29 +0100

    added check for different email except owns'

--- src/broodjes2/TeLaet/Repository/UserRepository.php
+++ src/broodjes2/TeLaet/Repository/UserRepository.php
@@ -3,6 +3,8 @@
 namespace broodjes2\TeLaet\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use broodjes2\TeLaet\Entities\User;
+use broodjes2\TeLaet\Entities\Constants\Entities;
 
 /**
  * Description of UserRepository
@@ -21,6 +23,19 @@ class UserRepository extends EntityRepository {
   
   public function findByUsername($username) {
     return $this->findOneBy(array('username' => $username));
+  }
+  
+  public function isEmailTaken($id, $email) {
+    $em = $this->getEntityManager();
+    $qb = $em->createQueryBuilder('u');
+    $dql = $qb->select('u')
+        ->from('broodjes2\TeLaet\Entities\User', 'u')
+        ->where('u.id != :id')
+        ->andWhere('u.email = :email')
+        ->setParameter('id', $id)
+        ->setParameter('email', $email);
+    $query = $dql->getQuery();
+    return count($query->execute()) < 1;
   }
   
 }
